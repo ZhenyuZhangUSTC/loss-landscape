6aa31b24a527582c5ec333a6448ef2363bdca961
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-08-23 23:33:52 +0800

    Update test.sh

--- script/test.sh
+++ script/test.sh
@@ -1,3 +1,5 @@
-mpirun -n 4 python plot_surface.py --x=-1:1:51 --y=-1:1:51 \
+mpirun -n 1 python plot_surface.py --x=-1:1:51 --y=-1:1:51 \
 --mpi --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter \
---ticket_dir $1
+--ticket_dir $1 --model_file $2
+
+
