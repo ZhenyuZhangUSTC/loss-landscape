d02059b94db64d5aebfb62935aecfbac7ccf154c
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-09-15 00:30:50 +0800

    x

--- script/launch.sh
+++ script/launch.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=$1 nohup python plot_trajectory.py \
     --model_folder $2 \
     --start_epoch 0 \
-    --max_epoch 99 
+    --max_epoch 99 > log_trajectory_$2_0915.out 2>&1 &
 
 
 
