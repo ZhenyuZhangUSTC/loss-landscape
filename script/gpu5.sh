329cd5878d761d201cddfef6e1b72b3cf1365b42
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-09-17 16:58:12 +0800

    x

--- script/gpu5.sh
+++ script/gpu5.sh
@@ -1,5 +1,5 @@
-bash script/plot_surface_adv.sh -20 70 -10 5 cifar_torch_data10
-bash script/plot_surface_adv.sh -20 90 -20 10 cifar_torch_data100
-bash script/plot_surface_adv.sh -5 25 -8 20 cifar_zico_data1
-bash script/plot_surface_adv.sh -20 130 -10 8 cifar_zico_data10
-bash script/plot_surface_adv.sh -30 160 -10 25 cifar_zico_data100
+bash script/plot_surface_adv.sh -20 70 -10 5 cifar_torch_data10 adv_data_torch_data10.h5
+bash script/plot_surface_adv.sh -20 90 -20 10 cifar_torch_data100 adv_data_torch_data100.h5
+bash script/plot_surface_adv.sh -5 25 -8 20 cifar_zico_data1 adv_data_zico_data1.h5
+bash script/plot_surface_adv.sh -20 130 -10 8 cifar_zico_data10 adv_data_zico_data10.h5
+bash script/plot_surface_adv.sh -30 160 -10 25 cifar_zico_data100 adv_data_zico_data100.h5
