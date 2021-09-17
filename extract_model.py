0be12d0187ad6543fc360928be62025069448e43
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-09-17 21:35:19 +0800

    Update extract_model.py

--- extract_model.py
+++ extract_model.py
@@ -6,12 +6,13 @@ import numpy as np
 all_path_list = np.loadtxt('file_path.txt', dtype=np.str)
 print(len(all_path_list))
 for file_path in all_path_list:
-    print(file_path)
     source_path = os.path.join(file_path[len('mini_cps/'):], 'checkpoint.pth.tar')
-    target_path_head = os.path.join('target_model_0917', file_path[len('mini_cps/'):])
-    os.makedirs(target_path_head, exist_ok=True)
-    target_path = os.path.join(target_path_head, 'checkpoint.pth.tar')
-    shutil.copyfile(source_path, target_path)
+    if os.path.exists(source_path):
+        print('Exist: ', file_path)
+        target_path_head = os.path.join('target_model_0917', file_path[len('mini_cps/'):])
+        os.makedirs(target_path_head, exist_ok=True)
+        target_path = os.path.join(target_path_head, 'checkpoint.pth.tar')
+        shutil.copyfile(source_path, target_path)
 
 
 
