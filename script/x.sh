c45617a71be4d3a11fb7d13e64b2861012538942
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-09-25 17:15:09 +0800

    Create x.sh

---
+++ script/x.sh
@@ -0,0 +1,20 @@
+python h52vtp.py --surf_file clean_data_nips_data10_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file clean_data_zico_data10_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file clean_data_torch_data10_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_nips_data10_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_zico_data10_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_torch_data10_newfinal.h5 --surf_name train_loss --zmax 10 --log
+
+python h52vtp.py --surf_file clean_data_nips_data100_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file clean_data_zico_data100_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file clean_data_torch_data100_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_nips_data100_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_zico_data100_newfinal.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_torch_data100_newfinal.h5 --surf_name train_loss --zmax 10 --log
+
+python h52vtp.py --surf_file clean_data_nips_data1_new3.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file clean_data_zico_data1_new3.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file clean_data_torch_data1_new3.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_nips_data1_new3.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_zico_data1_new3.h5 --surf_name train_loss --zmax 10 --log
+python h52vtp.py --surf_file adv_data_torch_data1_new3.h5 --surf_name train_loss --zmax 10 --log
