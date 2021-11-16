57dcdf3ed877fb35ce770b50af8c414a9308c8d9
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-11-17 03:58:29 +0800

    Update plt.sh

--- script/plt.sh
+++ script/plt.sh
@@ -1,4 +1,4 @@
 python plot_surface_backdoor.py --x=-1:1:51 --y=-1:1:51 --arch $1 --dataset $2 --data $3 \
     --model_file $4 --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter \
-    --plot 
+    --plot --upper_right
 
