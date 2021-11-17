c8aab9ac76d5f9d54d3c6a64dc38603e6f15c36e
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2021-11-17 12:39:44 +0800

    Update plt.sh

--- script/plt.sh
+++ script/plt.sh
@@ -1,4 +1,3 @@
-CUDA_VISIBLE_DEVICES=$5 nohup python plot_surface_backdoor.py --x=-1:1:51 --y=-1:1:51 --arch $1 --dataset $2 --data $3 \
+CUDA_VISIBLE_DEVICES=$5 python plot_surface_backdoor.py --x=-1:1:51 --y=-1:1:51 --arch $1 --dataset $2 --data $3 \
     --model_file $4 --cuda --dir_type weights --xignore biasbn --xnorm filter --yignore biasbn --ynorm filter \
-    --plot --upper_right --seed $6 --surf_file $7 > log_$7.out 2>&1 &
-
+    --plot --upper_right --seed $6 --surf_file $7 
